ed851348b33157bb764ccdfe1cf760f1f364633a
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-25 23:46:02 +0300

    Lesson 2 - Perceptron - Pattern Classification Capabilities

--- Neural networks in Python. Lessons/Lesson 1.py
+++ Neural networks in Python. Lessons/Lesson 1.py
@@ -7,14 +7,14 @@ def go(house, rock, attr):
     x = np.array([house, rock, attr])
     w11 = [0.3, 0.3, 0]
     w12 = [0.4, -0.5, 1]
-    weight1 = np.array([w11, w12]) # матрица 2*3
-    weight2 = np.array([-1, 1]) # еектор 1*3
+    weight1 = np.array([w11, w12]) # matrix 2*3
+    weight2 = np.array([-1, 1]) # vector 1*3
 
-    sum_hidden = np.dot(weight1, x)    # вычисляем сумму на входах нейронов скрытого слоя
-    print("Значения сумм на нейронах скрытого слоя: "+str(sum_hidden))
+    sum_hidden = np.dot(weight1, x)    # calculate the sum at the inputs of neurons in the hidden layer
+    print("The values of the sums on the neurons of the hidden layer: "+str(sum_hidden))
 
     out_hidden = np.array([act(x) for x in sum_hidden])
-    print("Значения на выходах нейронов скрытого слоя: "+str(out_hidden))
+    print("Values at the outputs of neurons in the hidden layer: "+str(out_hidden))
 
     sum_end = np.dot(weight2, out_hidden)
     y = act(sum_end)
@@ -27,6 +27,6 @@ attr = 1
 
 res = go(house, rock, attr)
 if res == 1:
-    print("Ты мне нравишься")
+    print("I like you")
 else:
-    print("Созвонимся")
+    print("Let's call")
